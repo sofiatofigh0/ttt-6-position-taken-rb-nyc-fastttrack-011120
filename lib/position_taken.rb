81fba70375008a509d5db9e9500872909f376821
sofia tofigh <sst2140@columbia.edu>
2020-03-06 02:25:51 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,11 +1,11 @@
-board= [" "," "," "," "," "," "," "," "," "] 
+board = [" "," "," "," "," "," "," "," "," "] 
 
 def position_taken(board,index)
   if board == "" ["","","","","","","","",""]
    puts false
   elsif board == nil
     puts false
-    
+  end
 end
 
-position_taken(board= "X","","","","","","","","")
+position_taken(board = "X","","","","","","","","")
